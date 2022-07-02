40d85129327374a3f32bf09dc82ac97705cd5e6f
samcodesign <samyben152@gmail.com>
2022-07-02 14:27:19 +0200

    .

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController 
     def index
+        flash.now[:notice] = "Logged in Succesfully"
+        flash.now[:alert] = "Invalid Log in. Please try again later"
     end
 end
